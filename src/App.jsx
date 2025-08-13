81cf2e43c07722aba465e0c8650a3ace733d88e2
SEGNI MERGA <segnimerga001@gmail.com>
2025-08-13 22:01:00 +0300

    other file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 
